403a549d61b7ba3567a7a00b7e91197b86195b67
kunalkushwaha <kunal.kushwaha@gmail.com>
2013-05-13 12:12:20 +0530

    added tweet score logic

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -11,9 +11,14 @@ def hw(sent_file,tweet_file):
 #    print scores.items() + '\n' # Print every (term, score) pair in the dictionary)
     
     for tweet in tweet_file:
+	    tweet_score = 0
 	    text_start_offset = tweet.find("text") # extract the start position of text + 6
 	    text_end_offset = tweet.find("source") # extract the end position of text - 2
-	    print tweet[text_start_offset+6:text_end_offset-2] + '\n'
+	    print '\n' +tweet[text_start_offset+6:text_end_offset-2]
+	    tweet_text = tweet[text_start_offset+6:text_end_offset-2].split()
+	    for word in tweet_text :
+		    tweet_score = tweet_score + scores.get(word,0) #cacluate the tweet score
+	    print "tweet-score " +  str(tweet_score)
 
 
 def lines(fp):
